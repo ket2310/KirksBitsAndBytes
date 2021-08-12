fbf52960b9668106cb9b3312c988450fc5f32b12
Kirk Thomas <dablumaroon@gmail.com>
2021-08-12 18:12:55 -0400

    Added image for readme

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 let sequelize;
 const Sequelize = require('sequelize')
 
+// Enable access to .env variables
+require('dotenv').config();
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
